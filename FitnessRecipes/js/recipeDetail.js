d5bba559fa947ee34af7493b0fbc107f7a6893aa
Arnstein Johansen <arnstej@gmail.com>
2013-04-06 14:39:43 +0200

    Before section refactoring

--- FitnessRecipes/js/recipeDetail.js
+++ FitnessRecipes/js/recipeDetail.js
@@ -13,4 +13,16 @@
             $.post("/meal/DeleteMeal?id=" + mealId);
         });
     });
-}).call(this);
+    (function (e, t, n) {
+        "use strict";
+        e.fn.foundationTabs = function (n) {
+            var r = e.extend({
+callback: e.noop            }, n), i = function (t) {
+                var n = t.closest("dl").find("dd.active"), r = t.children("a").attr("href") + "Tab";
+                r = r.replace(/^.+#/, "#") , n.removeClass("active") , t.addClass("active") , e(r).closest(".tabs-content").children("li").removeClass("active").hide() , e(r).css("display", "block").addClass("active");
+            };
+            e(document).on("click.fndtn", "dl.tabs dd a", function (t) {
+                i(e(this).parent("dd"));
+            }) , t.location.hash && (i(e('a[href="' + t.location.hash + '"]').parent("dd")) , r.callback());
+        };
+    })}).call(this);
--- FitnessRecipes/js/recipeDetail.ts
+++ FitnessRecipes/js/recipeDetail.ts
@@ -17,5 +17,11 @@
         $.post("/meal/DeleteMeal?id=" + mealId);
     });
   });
-
+(function(e,t,n)
+{
+    "use strict";e.fn.foundationTabs=function(n){
+        var r=e.extend({callback:e.noop},n),i=function(t){
+            var n=t.closest("dl").find("dd.active"),r=t.children("a").attr("href")+"Tab";r=r.replace(/^.+#/,"#"),n.removeClass("active"),t.addClass("active"),e(r).closest(".tabs-content").children("li").removeClass("active").hide(),e(r).css("display","block").addClass("active")};e(document).on("click.fndtn","dl.tabs dd a",function(t){i(e(this).parent("dd"))}),t.location.hash&&(i(e('a[href="'+t.location.hash+'"]').parent("dd")),r.callback())
+    }
+})
 }).call(this);
